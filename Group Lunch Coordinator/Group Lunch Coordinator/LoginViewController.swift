18266395adaf8dde6cac3ef4e09352cfe4796a3a
morgan smith <mssmitty07@gmail.com>
2019-12-19 21:10:32 -0500

    added login and detail storyboard screens

---
+++ Group Lunch Coordinator/Group Lunch Coordinator/LoginViewController.swift
@@ -0,0 +1,37 @@
+//
+//  LoginViewController.swift
+//  Group Lunch Coordinator
+//
+//  Created by Morgan Smith on 12/19/19.
+//  Copyright © 2019 Morgan Smith. All rights reserved.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    @IBOutlet weak var userNameText: UITextField!
+    
+    @IBOutlet weak var checkLunchTime: UIButton!
+    
+    @IBOutlet weak var joinLunch: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
