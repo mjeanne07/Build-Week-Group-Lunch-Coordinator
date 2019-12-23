b1334e84898027c3dc13a14832608605acb33624
sal <salmanamer@gmail.com>
2019-12-23 12:26:07 -0800

    Create tableviewcell - link to class created RestaurauntTableViewController created Restaurants.swift

---
+++ Group Lunch Coordinator/Group Lunch Coordinator/Model/Restaurants.swift
@@ -0,0 +1,16 @@
+//
+//  Restaurants.swift
+//  Group Lunch Coordinator
+//
+//  Created by SenorLappy on 23/12/19.
+//  Copyright © 2019 Morgan Smith. All rights reserved.
+//
+//Restaurant selection: chillis, pf changs, california pizza kitchen, cheesecake factory, Yard house
+
+import Foundation
+
+struct Restaurant: Codable {
+    var name: String
+    var details: String
+}
+
---
+++ Group Lunch Coordinator/Group Lunch Coordinator/Views/RestaurauntTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  RestaurauntTableViewCell.swift
+//  Group Lunch Coordinator
+//
+//  Created by SenorLappy on 23/12/19.
+//  Copyright © 2019 Morgan Smith. All rights reserved.
+//
+
+import UIKit
+
+class RestaurauntTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
